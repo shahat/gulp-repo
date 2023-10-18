1a7479e3f1672687e8fd3e9a1d041ef2b0850d4a
shahat <me6847570@gmail.com>
2023-10-18 16:05:03 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,89 @@
+const { src, dest, series, parallel, watch } = require("gulp");
 
-const { src, dest, series , watch,parallel} = require('gulp');
+const globs = {
+  html: "project/**/*.html",
+  css: "project/css/**/*.css",
+  js: "project/js/**/*.js",
+  img: "project/pics/*",
+};
 
-const globs={
-    html:"project/**/*.html",
-    css:"project/css/**/*.css",
-    js:"project/js/**/*.js",
-    img:"project/pics/*"
-}
-
+// ========================= HTML optimization =========================
 
 const htmlmin = require("gulp-html-minifier-terser");
-// html task
+
 function htmlTask() {
-    //read file
-   return src(globs.html)
-   //minfiy
-   .pipe(htmlmin({collapseWhitespace:true,removeComments:true}))
-    // move to dist
-    .pipe(dest("dist"))
+  return src(globs.html)
+    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
+    .pipe(dest("dist"));
 }
 
-exports.h= htmlTask
+exports.html = htmlTask; // Export the HTML task
 
+// ========================= CSS optimization =========================
 
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
 
-const concat =require("gulp-concat")
-const cleanCSS = require('gulp-clean-css');
 function cssTask() {
-    // read files
-   return src(globs.css)
-    // concat to one file
+  return src(globs.css)
     .pipe(concat("style.min.css"))
-    // minify
     .pipe(cleanCSS())
-    // move to dist
-    .pipe(dest("dist/assets/css"))
+    .pipe(dest("dist/assets/css"));
 }
 
+exports.css = cssTask; // Export the CSS task
 
-exports.css =cssTask
+// ========================= js optimization =========================
 
+const terser = require("gulp-terser");
 
-const terser = require('gulp-terser');
 function jsTask() {
-    return src(globs.js,{sourcemaps:true})
+  return src(globs.js, { sourcemaps: true })
     .pipe(concat("script.min.js"))
     .pipe(terser())
-    .pipe(dest("dist/assets/js" ,{sourcemaps:"."}))
+    .pipe(dest("dist/assets/js", { sourcemaps: "." }));
 }
 
+exports.js = jsTask; // Export the JS task
 
-exports.js =jsTask
-const optimizeImages =require("gulp-optimize-images");
+// ========================= image optimization =========================
+
+const optimizeImages = require("gulp-optimize-images");
+
 function imgTask() {
-    
-    return src(globs.img)
-    .pipe(optimizeImages({compressOptions:{
-        jpeg: {
+  return src(globs.img)
+    .pipe(
+      optimizeImages({
+        compressOptions: {
+          jpeg: {
             quality: 50,
             progressive: true,
-        },
-        png: {
+          },
+          png: {
             quality: 90,
             progressive: true,
             compressionLevel: 6,
-        },
-        webp: {
+          },
+          webp: {
             quality: 80,
+          },
         },
-    }}))
-    .pipe(dest('dist/assets/images'))
-}
-exports.img = imgTask
-
-// generate image sprite
-var spritesmith = require('gulp.spritesmith');
-function imgSprite() {
-    // this will generate a sprite folder in project with all-in-one.png and styleForSprite.css
-    return  src(globs.img)
-    .pipe(spritesmith({cssName:"styleForSprite.css",imgName:"all-in-one.png"}))
-    .pipe(dest("project/sprite"))
+      })
+    )
+    .pipe(dest("dist/assets/images"));
 }
 
-exports.sprite= imgSprite
+exports.img = imgTask; // Export the image optimization task
 
-function watchTask(){
-    watch(globs.html,htmlTask)
-    watch(globs.css,cssTask)
-    watch(globs.js,jsTask)
-    watch(globs.img,imgTask)
+// Watch task
+function watchTask() {
+  watch(globs.html, htmlTask);
+  watch(globs.css, cssTask);
+  watch(globs.js, jsTask);
+  watch(globs.img, imgTask);
 }
 
-function dummyTask(done){
-    // logic
-    console.log("test !");
-    done()
-}
+exports.watch = watchTask; // Export the watch task
 
-//default //gulp
-exports.default= series( parallel(  htmlTask, cssTask ,jsTask, imgTask ),dummyTask,watchTask )
-
-/* 
-function task1() {
-    // code
-    return Promise.resolve()
-}
-
-//named export
-exports.t1 =task1 //gulp t1
-
-//defualt export
-exports.default=  task1 //gulp */
+// Default task
+exports.default = series(parallel(htmlTask, cssTask, jsTask, imgTask));
